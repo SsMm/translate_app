d697677a5de48168aa617bd3266f11b1a43f391e
zhouguangyue <zhouguangyue666>
2018-01-02 19:21:34 +0800

    1.0

--- Translate/app/src/main/java/com/zgy/translate/global/GlobalKey.java
+++ Translate/app/src/main/java/com/zgy/translate/global/GlobalKey.java
@@ -7,13 +7,16 @@ package com.zgy.translate.global;
 public class GlobalKey {
 
     //翻译
-    public static final String TRANS_APP_ID = "20171211000103990";
-    public static final String TRANS_SECURITY_KEY = "t4MX3COL6zfah7NgbFT_";
+    public static final String TRANS_APP_ID = "20171211000103990"; //20171229000110420
+    public static final String TRANS_SECURITY_KEY = "t4MX3COL6zfah7NgbFT_"; //C1fhOTym7Kt7M5MMZCrQ
 
     //合成
-    public static final String TTS_APP_ID = "8535996";
+    public static final String TTS_APP_ID = "8535996"; //10615933
 
-    public static final String TTS_APP_KEY = "MxPpf3nF5QX0pndKKhS7IXcB";
+    public static final String TTS_APP_KEY = "MxPpf3nF5QX0pndKKhS7IXcB"; //I9p4xIw5MCfGmE76dDzScXVD
 
-    public static final String TTS_SECURITY_KEY = "7226e84664474aa098296da5eb2aa434";
+    public static final String TTS_SECURITY_KEY = "7226e84664474aa098296da5eb2aa434"; //Huu9Do2Gl75Hnz2xgzVtYCSkk3TgN2NC
+
+    //友盟key
+    public static final String UM_APP_KEY = "5a4b61148f4a9d294200008e";
 }

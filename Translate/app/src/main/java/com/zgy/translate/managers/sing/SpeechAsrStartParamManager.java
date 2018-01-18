75dca571f5af5a0d8c95faf647c43b953059e45e
zhouguangyue <zhouguangyue666>
2018-01-18 18:29:56 +0800

    1.0

--- Translate/app/src/main/java/com/zgy/translate/managers/sing/SpeechAsrStartParamManager.java
+++ Translate/app/src/main/java/com/zgy/translate/managers/sing/SpeechAsrStartParamManager.java
@@ -1,6 +1,6 @@
 package com.zgy.translate.managers.sing;
 
-import com.baidu.speech.asr.SpeechConstant;
+
 import com.zgy.translate.global.GlobalInit;
 import com.zgy.translate.global.GlobalParams;
 import com.zgy.translate.managers.CacheManager;
@@ -16,14 +16,14 @@ public class SpeechAsrStartParamManager {
     
     private static Map<String, Object> map;
 
-    static {
+   /* static {
         map = new HashMap<>();
         map.put(SpeechConstant.ACCEPT_AUDIO_DATA, false);
         map.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, true); //音量
         map.put(SpeechConstant.VAD_ENDPOINT_TIMEOUT, 0);
         map.put(SpeechConstant.VAD, "dnn");
         map.put(SpeechConstant.DISABLE_PUNCTUATION, true); //标点
-    }
+    }*/
 
     private static SpeechAsrStartParamManager manager;
 
@@ -40,27 +40,27 @@ public class SpeechAsrStartParamManager {
 
     /**中文输入*/
     public SpeechAsrStartParamManager createCN(){
-        map.put(SpeechConstant.PID, "1537");
+        //map.put(SpeechConstant.PID, "1537");
         return manager;
     }
 
     /**英文输入*/
     public SpeechAsrStartParamManager createEN(){
-        map.put(SpeechConstant.PID, "1737");
+        //map.put(SpeechConstant.PID, "1737");
         return manager;
     }
 
     /**麦克风输入源*/
     public SpeechAsrStartParamManager createVoide(){
-        if(map.containsKey(SpeechConstant.IN_FILE)){
+        /*if(map.containsKey(SpeechConstant.IN_FILE)){
             map.remove(SpeechConstant.IN_FILE);
-        }
+        }*/
         return manager;
     }
 
     /**耳机输入源*/
     public SpeechAsrStartParamManager createBlue(String path){
-        map.put(SpeechConstant.IN_FILE, path);
+        //map.put(SpeechConstant.IN_FILE, path);
         return manager;
     }
 
